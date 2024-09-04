83c6b4d79a6298157a45c405cc1ce90d70978b80
REDMOND\sdeguchi <sdeguchi@microsoft.com>
2024-09-04 07:59:57 -0700

    Adding alz archetypes to template architecture definition

--- modules/template_architecture_definition/locals.tf
+++ modules/template_architecture_definition/locals.tf
@@ -7,6 +7,18 @@ locals {
   template_file_path = "${var.starter_module_folder_path}/../Common/template_architecture_definition/templates/${var.architecture_definition_name}.alz_architecture_definition.json.tftpl"
   enable_alz         = var.enable_alz
 
+  # ALZ archetypes
+  alz_root = "\"root\""
+  alz_platform = "\"platform\""
+  alz_landing_zone = "\"landing_zone\""
+  alz_decommissioned = "\"decommissioned\""
+  alz_sandboxes = "\"sandboxes\""
+  alz_corp = "\"corp\""
+  alz_online = "\"online\""
+  alz_management = "\"management\""
+  alz_connectivity = "\"connectivity\""
+  alz_identity = "\"identity\""
+  
   template_vars = {
     architecture_definition_name            = var.architecture_definition_name
     root_management_group_id                = "${local.default_prefix}${local.default_postfix}"
@@ -23,7 +35,16 @@ locals {
     confidential_online_management_group_id = "${local.default_prefix}-landingzones-confidential-online${local.default_postfix}"
 
     # example of how to add dynamic values to the template archetypes
-    root_archetypes = var.enable_alz ? "\"fsi_root\", \"root\"" : "\"fsi_root\""
+    root_archetypes = var.enable_alz ? local.alz_root : ""
+    platform_archetypes = var.enable_alz ? local.alz_platform : ""
+    landing_zone_archetypes = var.enable_alz ? local.alz_landing_zone : ""
+    decommissioned_archetypes = var.enable_alz ? local.alz_decommissioned : ""
+    sandboxes_archetypes = var.enable_alz ? local.alz_sandboxes : ""
+    corp_archetypes = var.enable_alz ? local.alz_corp : ""
+    online_archetypes = var.enable_alz ? local.alz_online : ""
+    management_archetypes = var.enable_alz ? local.alz_management : ""
+    connectivity_archetypes = var.enable_alz ? local.alz_connectivity : ""
+    identity_archetypes = var.enable_alz ? local.alz_identity : ""
     
     # sample snippet from architecture definition
     # "management_groups": [
